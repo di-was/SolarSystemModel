b1b54b73c55a7e429eb88b992825259b74567344
di-was <diwasdahal109@gmail.com>
2023-07-01 10:33:59 +0545

    refi

--- js/planets.js
+++ js/planets.js
@@ -1,36 +1,11 @@
-export const primaryPlanetSkeleton = {
-    color: 'blue',
-    mass: 5,
-    radius: 6,
-    xaxis: window.innerWidth / 2,
-    yaxis: window.innerHeight / 2,
-    startingAngle: 0,
-    endingAngle: 2 * Math.PI,
-    counterClockwise: false,
-    velocity: {xaxis: 0, yaxis: 0},
-    acceleration: {xaxis: 0, yaxis: 0}
-}
+import { Vector } from "./Vector.js"
 
-export function makePlanet(color, mass, radius, xaxis, yaxis) {
-    if (color==undefined && mass==undefined && radius==undefined && xaxis==undefined && yaxis==undefined) {
-        return structuredClone(primaryPlanetSkeleton)
-    } else {
-        let secondaryPlanetSekelton = structuredClone(primaryPlanetSkeleton)
-        if (color !== undefined) {
-            secondaryPlanetSekelton.color = color
-        }
-        if (mass !== undefined) {
-            secondaryPlanetSekelton.mass = mass
-        }
-        if (radius !== undefined) {
-            secondaryPlanetSekelton.radius = radius
-        }
-        if (xaxis !== undefined) {
-            secondaryPlanetSekelton.xaxis = xaxis
-        }
-        if (yaxis !== undefined) {
-            secondaryPlanetSekelton.yaxis = yaxis
-        }
-        return secondaryPlanetSekelton
+export class Planets {
+    constructor(coordinates, radius) {
+        this.coordinates = coordinates
+        this.radius = radius
+        this.mass = radius * radius * Math.PI
+        this.velocity = new Vector()
+        this.acceleration = new Vector()
     }
 }
